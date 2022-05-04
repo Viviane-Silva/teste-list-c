50ba539e57cce68bbff872b8c621c7ca508e9156
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-05-04 20:09:06 -0300

    Introdução

---
+++ teste-lista/teste-lista/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> MinhaLista = new List<int>();
+
+
+        Console.WriteLine("Digite um número: ");
+        //adicionar números a lista
+        int i = 0;
+        while ( i  < 5)
+        {
+            MinhaLista.Add(int.Parse(Console.ReadLine()));
+            i++;
+        }
+        //MinhaLista.Add(int.Parse(Console.ReadLine()));
+
+        Console.WriteLine("Você digitou esse números: " + String.Join(",",MinhaLista));
+    }
+}
